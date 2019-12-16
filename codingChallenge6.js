3f301b6d04e706ce59f4d0eb88328b9c54891305
Sanchit Taliyan <taliyan.1@iitj.ac.in>
2019-12-16 15:51:23 +0530

    1. Added 2 dice game. 2. Added a new rule.

--- codingChallenge6.js
+++ codingChallenge6.js
@@ -1,6 +1,6 @@
 // pig-Game//////////////////////////////////////////
 
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, prevDice1, prevDice2, gamePlaying;
 
 init();
 
@@ -10,25 +10,35 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
   if(gamePlaying) {
     //1. generate a random number
-    var dice = Math.floor(Math.random() * 6) +1;
+    var dice1 = Math.floor(Math.random() * 6) +1;
+    var dice2 = Math.floor(Math.random() * 6) +1;
 
     //2. Display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    var diceDOM1 = document.getElementById('dice-1');
+    var diceDOM2 = document.getElementById('dice-2');
+    diceDOM1.style.display = 'block';
+    diceDOM1.src = 'dice-' + dice1 + '.png';
+    diceDOM2.style.display = 'block';
+    diceDOM2.src = 'dice-' + dice2 + '.png';
     //document.getElementById('current-' + activePlayer).textContent = dice;
 
 
     //3. Update the round score IF rolled number was NOT a 1
-    if(dice !== 1) {
+    if((dice1 === 6 && prevDice1 === 6) || (dice2 === 6 && prevDice2 === 6)) {
+      //current player looses all the scores
+      scores[activePlayer] = 0;
+      document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+      changePlayer();
+    }else if(dice1 !== 1 && dice2 != 1) {
       //Add Score
-      roundScore += dice;
+      roundScore += dice1 + dice2;
       document.getElementById('current-' + activePlayer).textContent = roundScore;
-    }
-    else {
+    }else {
       //Update the UI and Player
       changePlayer();
     }
+    prevDice1 = dice1;
+    prevDice2 = dice2;
   }
 });
 
@@ -39,10 +49,23 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     scores[activePlayer] += roundScore;
     document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
 
+    //variable to store the final score
+    var input = document.querySelector('.final-score').value;
+    var winningScore;
+
+    // Undefined, 0, Null, and "" are COERCED to false
+    //Anything else is COERCED to true
+    if(input) {
+      winningScore = input;
+    }else {
+      winningScore = 100;
+    }
+
     //check If player won the game
-    if(scores[activePlayer] >= 20) {
+    if(scores[activePlayer] >= winningScore) {
       document.getElementById('name-' + activePlayer).textContent = 'WINNER!';
-      document.querySelector('.dice').style.display = 'none';
+      document.getElementById('dice-1').style.display = 'none';
+      document.getElementById('dice-2').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
       roundScore = 0;
@@ -65,7 +88,8 @@ function changePlayer () {
   //document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 }
 
 // here init is a call back function bcz this is a function not called by us but by another function
@@ -78,7 +102,8 @@ function init() {
   activePlayer = 0;
   gamePlaying = true;
 
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
   document.getElementById('current-0').textContent = '0';
